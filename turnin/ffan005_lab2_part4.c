780b108361cbe6d96c13b2db7eb383f7262c18d4
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-01 15:13:20 -0700

    Update ffan005_lab2_part4.c

--- turnin/ffan005_lab2_part4.c
+++ turnin/ffan005_lab2_part4.c
@@ -17,11 +17,16 @@ unsigned char tmpA = 0x00;
 unsigned char tmpB = 0x00;
 unsigned char tmpC = 0x00;
 unsigned char tmpD = 0x00;
-unsigned char tWeight = 0x00;
+//unsigned char tWeight = 0x00;
 unsigned char tmpE = 0x00;
-  
+unsigned short tWeight = 0x00;
+unsigned char tmpF = 0x00;
+
 DDRA = 0x00; PORTA = 0xFF;
 DDRD = 0xFF; PORTD = 0x00; // since this time we want port C to be the output
+DDRB = 0x00; PORTB = 0xFF;
+DDRC = 0x00; PORTC = 0xFF;
+
 while(1) {
 tmpA = PINA;
 tmpB = PINB;
@@ -31,11 +36,11 @@ tWeight = tmpA + tmpB + tmpC;
   tmpD = 0x01;  //PD0 = 1;
 }
   if(((tmpA - tmpC) > 80) || ((tmpC - tmpA) > 80)){
-  tmpE = 0x02;  
+  tmpE = 0x02;  //PD1 = 1;
 }
-  
-PORTD = (tmpD | tmpE | tWeight);
+
+tmpF = tWeight & 0xFC; //to keep the last two digit 0 for total weight
+PORTD = (tmpD | tmpE | tmpF);
 }
 return 0;
-
 }
